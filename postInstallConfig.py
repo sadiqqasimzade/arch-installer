0e256b305800af055e0df5d8dd11ce808c7be04a
sadiqqasimzade <tu6w5xs1c@code.edu.az>
2024-05-01 23:26:50 +0400

    .

--- postInstallConfig.py
+++ postInstallConfig.py
@@ -6,6 +6,11 @@ def main():
     os.system("chsh -s /usr/bin/fish")
     os.system("sudo chmod -R 700 ~/.config/*")
 
+    os.system("cp -r config/* ~/.config/")
+    os.system("cp Xresources ~/.Xresources")
+    os.system("cp gtkrc-2.0 ~/.gtkrc-2.0")
+    os.system("cp xinitrc ~/.xinitrc")
+    os.system("cp -r bin/ ~/")
 
 if __name__ == "__main__":
     main()
